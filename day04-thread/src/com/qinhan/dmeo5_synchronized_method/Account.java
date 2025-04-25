ab593b18d8468224d42efd3aa7fb0fee3dc1b760
qinhan <3091533438@qq.com>
2025-04-25 15:03:14 +0800

    feat(day04-thread): 通过使用同步方法的方式实现线程同步方法示例

---
+++ day04-thread/src/com/qinhan/dmeo5_synchronized_method/Account.java
@@ -0,0 +1,26 @@
+package com.qinhan.dmeo5_synchronized_method;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    private String cardId;
+    private double money;
+
+    // 小明和小红都到这里来取钱
+    public synchronized void drawMoney(double money) {
+        String name = Thread.currentThread().getName();
+        // 判断余额是否足够
+        if (this.money>=money){
+            System.out.println(name+"取钱成功，取了"+money+"元");
+            this.money-=money;
+            System.out.println(name+"取钱成功，余额为"+this.money);
+        }else {
+            System.out.println(name+"取钱失败，余额不足");
+        }
+    }
+}
