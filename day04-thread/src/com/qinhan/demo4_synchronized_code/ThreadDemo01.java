ebb182fd72ee0678045db8fa57c46f33e020989b
qinhan <3091533438@qq.com>
2025-04-25 14:55:13 +0800

    feat(day04-thread): 通过同步代码块的方式实现线程同步的取款操作

---
+++ day04-thread/src/com/qinhan/demo4_synchronized_code/ThreadDemo01.java
@@ -0,0 +1,14 @@
+package com.qinhan.demo4_synchronized_code;
+
+public class ThreadDemo01 {
+    public static void main(String[] args) {
+        // 目标：线程同步的方式一：同步代码块
+        // 1.设计一个账户类，用于创建小明和小红的共同账户对象，存入100000元
+        Account acc = new Account("ICBC-110",100000);
+
+        // 2.设计线程类，创建小明和小红两个线程，模拟小红和小明同时去一个账户取款100000
+        new DrawThread("小明",acc).start();
+        new DrawThread("小红",acc).start();
+
+    }
+}
