2046cb90b6fb8004d38e83335bf26741c0b9dbe5
qinhan <3091533438@qq.com>
2025-04-25 14:38:40 +0800

    feat(thread): 模拟线程安全问题
    - 新增 Account 类，表示银行账户
    - 新增 DrawThread 类，表示取款线程
    - 新增 ThreadDemo01 类，演示线程安全问题

---
+++ day04-thread/src/com/qinhan/threadsafe/DrawThread.java
@@ -0,0 +1,18 @@
+package com.qinhan.threadsafe;
+
+public class DrawThread extends Thread {
+
+    private Account acc;    // 记住线程对象要处理的账户对象
+
+
+    public DrawThread(String name, Account acc) {
+        super(name);
+        this.acc = acc;
+    }
+
+    @Override
+    public void run() {
+        // 取钱
+        acc.drawMoney(100000);
+    }
+}
