d9bf1957e41ff8783d008dfd614e0a8d810b4431
qinhan <3091533438@qq.com>
2025-04-25 16:26:50 +0800

    feat(day04-thread): 添加线程池执行 Callable 任务的示例
    - 新增 ExecutorServiceDemo02 类，演示如何创建线程池并执行 Callable 任务- 添加 MyCallable 类，实现 Callable 接口用于计算 1到指定数字的和
    - 示例代码展示了线程池的配置和使用，以及如何获取 Callable 任务的结果

---
+++ day04-thread/src/com/qinhan/demo7executorService/MyCallable.java
@@ -0,0 +1,21 @@
+package com.qinhan.demo7executorService;
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<String> {
+
+    private int n;
+    public MyCallable(int n){
+        this.n = n;
+    }
+    @Override
+    public String call() throws Exception {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return Thread.currentThread().getName()+"线程计算1-"+n+"的和是"+sum;
+    }
+
+
+}
