f61ca6dd4eb9ebf311dbbd1b142bde75521f1ece
qinhan <3091533438@qq.com>
2025-05-05 20:59:36 +0800

    feat(day06-junit-reflect-annotation-proxy): 添加代理模式示例代码
    - 新增 ProxyUtils 类实现代理对象创建功能
    - 添加 Star 类作为被代理的明星对象
    - 定义 StarService 接口作为明星行为规范- 编写 Test 类进行代理模式测试

---
+++ day06-junit-reflect-annotation-proxy/src/com/qinhan/dmeo4proxy/StarService.java
@@ -0,0 +1,7 @@
+package com.qinhan.dmeo4proxy;
+
+// 明星行为接口
+public interface StarService {
+    void sing(String name);
+    String dance();
+}
---
+++ day06-junit-reflect-annotation-proxy/src/com/qinhan/dmeo4proxy/Test.java
@@ -0,0 +1,15 @@
+package com.qinhan.dmeo4proxy;
+
+import java.lang.reflect.Proxy;
+
+public class Test {
+    public static void main(String[] args) {
+        // 目标：准备代理对象
+        // 1.准备一个明星对象：设计明星类
+        Star star = new Star("章若楠");
+        // 2.为章若楠创建一个专属的代理对象。
+        StarService proxy = ProxyUtils.createProxy(star);
+        proxy.sing("红昭愿");
+        System.out.println(proxy.dance());
+    }
+}
