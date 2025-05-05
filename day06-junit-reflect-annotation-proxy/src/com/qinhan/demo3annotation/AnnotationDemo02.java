301ac40e0057018c082e7dbfaeb59bc1c6ce8b05
qinhan <3091533438@qq.com>
2025-05-05 19:22:15 +0800

    feat(demo): 添加 MyTest1 注解和相关示例代码- 新增 MyTest1 注解，用于标记方法和成员变量- 创建 AnnotationDemo02 类，演示注解的使用方法 - 学习了两个注解@Target注解表示注解的作用目标 _ @Retention注解表示注解的保留策略

---
+++ day06-junit-reflect-annotation-proxy/src/com/qinhan/demo3annotation/AnnotationDemo02.java
@@ -0,0 +1,21 @@
+package com.qinhan.demo3annotation;
+
+//@MyTest1
+public class AnnotationDemo02 {
+
+    @MyTest1
+    private int age;
+
+    //@MyTest1
+    public AnnotationDemo02(){}
+
+    @MyTest1
+    public static void main(String[] args) {
+        // 目标:搞清楚元注解的作用
+    }
+
+    @MyTest1
+    public void getAgeTest(){
+
+    }
+}
